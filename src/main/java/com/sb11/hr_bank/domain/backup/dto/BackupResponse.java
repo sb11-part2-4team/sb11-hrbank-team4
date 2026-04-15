9b6059a0ae1a692e451dd9f8618e7af5d113e588
김명근 <rlaaudrms369@gmail.com>
2026-04-15 18:42:48 +0900

    rename: BackupDto -> BackupResponse로 파일 이름 변경

--- src/main/java/com/sb11/hr_bank/domain/backup/dto/BackupResponse.java
+++ src/main/java/com/sb11/hr_bank/domain/backup/dto/BackupResponse.java
@@ -4,7 +4,7 @@ import com.sb11.hr_bank.domain.backup.entity.BackupStatus;
 import com.sb11.hr_bank.domain.file.dto.FileResponse;
 import java.time.Instant;
 
-public record BackupDto(
+public record BackupResponse(
     Long id,
     String worker,
     Instant startedAt,
