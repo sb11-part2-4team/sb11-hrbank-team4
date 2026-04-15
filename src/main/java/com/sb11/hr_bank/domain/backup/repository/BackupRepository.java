fe9fcfc1dc71f6562701fe3efa1a018bf3f647e6
김명근 <rlaaudrms369@gmail.com>
2026-04-15 17:21:07 +0900

    feat: Backup 레이어(Controller, Service, Repository, Dto, Entity) 추가

---
+++ src/main/java/com/sb11/hr_bank/domain/backup/repository/BackupRepository.java
@@ -0,0 +1,12 @@
+package com.sb11.hr_bank.domain.backup.repository;
+
+
+import com.sb11.hr_bank.domain.backup.entity.Backup;
+import com.sb11.hr_bank.domain.backup.entity.BackupStatus;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BackupRepository extends JpaRepository<Backup, Long> {
+  Optional<Backup> findTopByStatusOrderByEndedAtDesc(BackupStatus status); // EndedAt 내림차순 정렬 가장 위 status를 확인
+
+}
