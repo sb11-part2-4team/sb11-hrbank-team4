cd7fe039d594214d69a25cc0d90bc326433d9d0f
김명근 <rlaaudrms369@gmail.com>
2026-04-16 14:20:11 +0900

    refactor: BackupRepository의 jpa 메서드 통합

--- src/main/java/com/sb11/hr_bank/domain/backup/repository/BackupRepository.java
+++ src/main/java/com/sb11/hr_bank/domain/backup/repository/BackupRepository.java
@@ -3,14 +3,11 @@ package com.sb11.hr_bank.domain.backup.repository;
 
 import com.sb11.hr_bank.domain.backup.entity.Backup;
 import com.sb11.hr_bank.domain.backup.entity.BackupStatus;
-import java.time.Instant;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface BackupRepository extends JpaRepository<Backup, Long> {
-  Optional<Backup> findTopByStatusOrderByEndedAtDesc(BackupStatus status); // EndedAt 내림차순 정렬 가장 위 status를 확인
 
-  @Query("SELECT MAX(endedAt) FROM Backup WHERE status = 'COMPLETED'")
-  Optional<Instant> findLastCompletedTime();
+  // EndedAt 내림차순 정렬 가장 위 status를 확인
+  Optional<Backup> findTopByStatusOrderByEndedAtDesc(BackupStatus status);
 }
