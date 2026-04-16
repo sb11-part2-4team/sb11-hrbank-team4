62bd60affc67bdfafc41f31e1d839b19e9af8c01
Linpield <top09100620@gmail.com>
2026-04-17 02:18:28 +0900

    feat: 공통 CursurPageResponseDto 추가

---
+++ src/main/java/com/sb11/hr_bank/global/dto/PageResponse.java
@@ -0,0 +1,46 @@
+package com.sb11.hr_bank.global.dto;
+
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Slice;
+
+public record PageResponse<T>(
+
+    List<T> content,
+    String nextCursor,
+    Long nextIdAfter,
+    int size,
+    Long totalElements,
+    boolean hasNext
+) {
+
+  static <T> PageResponse<T> fromPage(Page<T> page){
+
+    return new PageResponse<>(
+
+        page.getContent(),
+        null,
+        null,
+        page.getSize(),
+        page.getTotalElements(),
+        page.hasNext()
+
+    );
+
+
+  }
+  static <T> PageResponse<T> fromSlice(Slice<T> slice, String nextCursor, Long nextIdAfter){
+
+    return new PageResponse<>(
+
+        slice.getContent(),
+        nextCursor,
+        nextIdAfter,
+        slice.getSize(),
+        null,
+        slice.hasNext()
+    );
+  }
+
+
+}
