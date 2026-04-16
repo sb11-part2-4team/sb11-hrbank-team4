a7101654e2c60067b7e033a3e0c136941d0c3266
김명근 <rlaaudrms369@gmail.com>
2026-04-16 11:23:10 +0900

    feat: API 명세서 요구사항에 맞춘 Service 인터페이스 작성

--- src/main/java/com/sb11/hr_bank/domain/backup/service/BackupService.java
+++ src/main/java/com/sb11/hr_bank/domain/backup/service/BackupService.java
@@ -1,5 +1,15 @@
 package com.sb11.hr_bank.domain.backup.service;
 
+import com.sb11.hr_bank.domain.backup.dto.BackupResponse;
+
 public interface BackupService {
 
+  // 백업 실행
+  void startBackup(String worker);
+
+  // 백업 목록 조회
+  BackupResponse findAll();
+
+  // 가장 최신 백업 조회
+  BackupResponse findLatest();
 }
