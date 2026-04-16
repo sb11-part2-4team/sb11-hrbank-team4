16967b4d350f64a39d1f5308f0ac108680036d28
wuuujuuun <wuuujuuun@gmail.com>
2026-04-16 15:25:19 +0900

    feat: 부서 상세 조회 및 소속 직원 목록 반환 기능 추가

--- src/main/java/com/sb11/hr_bank/domain/department/controller/DepartmentController.java
+++ src/main/java/com/sb11/hr_bank/domain/department/controller/DepartmentController.java
@@ -1,12 +1,19 @@
 package com.sb11.hr_bank.domain.department.controller;
 
+import com.sb11.hr_bank.domain.department.dto.DepartmentRequest;
+import com.sb11.hr_bank.domain.department.dto.DepartmentResponse;
 import com.sb11.hr_bank.domain.department.entity.Department;
 import com.sb11.hr_bank.domain.department.service.DepartmentService;
-import com.sb11.hr_bank.domain.department.dto.DepartmentRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 이 클래스가 외부 요청을 받는곳
 @RequestMapping("/sb/hrbank/api/departments") // Swagger에 명시된 기본 주소를 설정
@@ -54,6 +61,11 @@ public class DepartmentController {
     departmentService.delete(id);
 
   // 삭제할 대상의 ID를 요청후 서비스에게 해당 번호의 부서를 삭제하라고 요청
+  }
+
+  @GetMapping("/{id}")
+  public DepartmentResponse getDepartmentDetail(@PathVariable Long id) {
+    return departmentService.getDepartmentDetail(id);
   }
 
   // 부서 목록 조회
