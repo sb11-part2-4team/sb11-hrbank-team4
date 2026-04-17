190533c3040a34cc1f9491f9ab42b8d842993359
김명근 <rlaaudrms369@gmail.com>
2026-04-17 10:47:02 +0900

    feat: 스케듈러를 활성화 시키는 @EnableScheduling을 포함하는 클래스 작성

--- src/main/java/com/sb11/hr_bank/domain/changelogs/repository/ChangeLogRepository.java
+++ src/main/java/com/sb11/hr_bank/domain/changelogs/repository/ChangeLogRepository.java
@@ -1,5 +1,10 @@
 package com.sb11.hr_bank.domain.changelogs.repository;
 
-public class ChangeLogRepository {
+import com.sb11.hr_bank.domain.changelogs.entity.ChangeLog;
+import java.time.Instant;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface ChangeLogRepository extends JpaRepository<ChangeLog, Long> {
+
+  boolean existsByCreatedAtAfter(Instant lastBackupTime);
 }
