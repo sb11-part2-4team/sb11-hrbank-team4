f8215e27edb0e46ad5ee64da8f61e3058633737f
김명근 <rlaaudrms369@gmail.com>
2026-04-21 11:58:43 +0900

    feat: 백업데이터 생성 시 API 응답에서 상태를 한글로 출력하도록 수정

--- src/main/java/com/sb11/hr_bank/domain/backup/entity/BackupStatus.java
+++ src/main/java/com/sb11/hr_bank/domain/backup/entity/BackupStatus.java
@@ -1,13 +1,17 @@
 package com.sb11.hr_bank.domain.backup.entity;
 
-import lombok.Getter;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.RequiredArgsConstructor;
 
-@Getter
 @RequiredArgsConstructor
 public enum BackupStatus {
   IN_PROGRESS("진행중"), COMPLETED("완료"), FAILED("실패"), SKIPPED("건너뜀");
 
   private final String description;
+
+  @JsonValue
+  public String getDescription() {
+    return description;
+  }
 }
 // Backup의 상태, 진행중(처리), 완료, 실패, 건너뜀(변경 이력이 없을 시)
